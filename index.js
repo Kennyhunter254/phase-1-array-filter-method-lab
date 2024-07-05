575bf132cb0d640d5e0a94042a5953db5598c9bd
Kennyhunter254 <kipronokevin566@gmail.com>
2024-07-05 12:08:49 +0300

    Array filter method lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution here
+ 
+
+// Function to find all drivers that match the passed in name
+function findMatching(drivers, name) {
+    return drivers.filter(driver =>
+      driver.toLowerCase() === name.toLowerCase()
+    );
+  }
+  
+  
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver =>
+      driver.toLowerCase().startsWith(letters.toLowerCase())
+    );
+  }
+  
+  
+  function matchName(drivers, name) {
+    return drivers.filter(driver =>
+      driver.name.toLowerCase() === name.toLowerCase()
+    );
+  }
+  
+  module.exports = {
+    findMatching,
+    fuzzyMatch,
+    matchName
+  };
+  
